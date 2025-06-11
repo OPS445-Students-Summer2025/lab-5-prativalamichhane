a2d3d87ec2ff8d231b214b2a9bf8501d0d853b91
Prativa Lamichhane <plamichhane2@myseneca.ca>
2025-06-11 16:51:20 -0400

    updated lab5a

--- lab5a.py
+++ lab5a.py
@@ -2,9 +2,7 @@
 # Author ID: plamichhane2
 
 def read_file_string(file_name):
-    """
-    Takes file_name as a string for a file name, returns its entire contents as a string.
-    """
+    # Takes file_name as string for a file name, returns its entire contents as a string
     try:
         with open(file_name, 'r') as file:
             return file.read()
@@ -12,10 +10,9 @@ def read_file_string(file_name):
         return "Error: File not found."
 
 def read_file_list(file_name):
-    """
-    Takes file_name as a string for a file name,
-    returns its entire contents as a list of lines without new-line characters.
-    """
+    
+    #Takes file_name as a string for a file name,
+    #returns its entire contents as a list of lines without new-line characters
     try:
         with open(file_name, 'r') as file:
             return [line.strip() for line in file.readlines()]
